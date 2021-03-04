425d5111e0040d86ae5fa0677ec3c4ad8487963b
pangbo <pangbo@rakuten.dhc.com>
2021-03-04 13:27:24 +0800

    redux

---
+++ redux.js
@@ -0,0 +1,30 @@
+function createStore(reducer){
+    let state = null;
+    const listeners = [];
+    const subscribe = (listener) => listeners.push(listener)
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener())
+    }
+    
+    dispatch();
+    return { getState, dispatch, subscribe}
+}
+
+const initalState = {
+    color: "red",
+    text: 'abc'
+}
+function themeReducer (state = initalState, action){
+    switch(action.type){
+        case 'CHANGE_COLOR':
+            return {...state, color: action.payload.color}
+        case 'B':
+            return {...state, text: action.payload.text}
+        default:
+            return state
+    }
+}
+
+const store = createStore(themeReducer )
+
